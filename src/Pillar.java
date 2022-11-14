964f14a9b357527ff730de0fbaac946072645fdc
Jered <Sinoffate@hotmail.com>
2022-11-14 15:27:35 -0800

    Revert "183743009 mock inventory"

---
+++ src/Pillar.java
@@ -0,0 +1,24 @@
+/**
+ * Pillar item class.
+ * @author james deal, jered wiegel
+ * @version 0.1
+ */
+public class Pillar extends Item {
+
+    /**
+     * Pillar creation.
+     * @param theName name of pillar.
+     */
+    public Pillar(final String theName) {
+        super(theName);
+    }
+
+    /**
+     * Returns name of item.
+     * @return name.
+     */
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
