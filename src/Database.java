aa9545caaaac627dda5da70a638f322992c8341d
hyunggilwoo <hyunggilswoo@gmail.com>
2022-11-20 08:37:41 -0800

    Updated method names + added delete(), update(), reset()
    I wanted to remove redundant naming in method calls.
    So I suggested to changing the name of  addItem(String) into
    add(String). Created a skeleton for reset()
    
    What is next:
    
    Test if database is updated with the current database class.

--- src/Database.java
+++ src/Database.java
@@ -1,25 +1,30 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import org.sqlite.SQLiteDataSource;
 
 /**
- * This class sets up SQLite database.
- * @author Jered Wiegel
- * @version 1.0
+ * This class sets up SQLite database. Database should be accessed by the Inventory and Hero.
+ * Inventory should be accessed by the entire inventory and Hero.
+ * 
+ * @author Jered Wiegel, Hyunggil Woo
+ * @version 1.1
  */
 public class Database {
 
-    private SQLiteDataSource ds = null;
+    private SQLiteDataSource myDs = null;
 
     /**
      * Constructor for Database.
+     * Database will contain non-null references.
      */
     public Database() {
         try {
-            ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:Project_Doom.db");
+            myDs = new SQLiteDataSource();
+            myDs.setUrl("jdbc:sqlite:Project_Doom.db");
         } catch (final Exception theEvent) {
             theEvent.printStackTrace();
             System.exit(0);
@@ -31,11 +36,14 @@ public class Database {
      * This method creates the tables in the database.
      */
     public void createTables() {
+        
+        //TODO: Does the number of items automatically increment?
         String query = "CREATE TABLE IF NOT EXISTS inventory ( " +
                 "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "NAME TEXT NOT NULL, " +
                 "TYPE TEXT NOT NULL )";
 
+        // query to create a table for a player
         String query2 = "CREATE TABLE IF NOT EXISTS player ( " +
                 "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "NAME TEXT NOT NULL, " +
@@ -44,55 +52,126 @@ public class Database {
                 "FOREIGN KEY (INVENTORY_ID) REFERENCES inventory(ID) )";
 
 
-        try (Connection connection = ds.getConnection();
+        try (Connection connection = myDs.getConnection();
              Statement statement = connection.createStatement()) {
-            int rv = statement.executeUpdate(query);
-            int rv2 = statement.executeUpdate(query2);
-            System.out.println("executeUpdate() returned " + rv);
-            System.out.println("executeUpdate() returned " + rv2);
+
+            statement.executeUpdate(query);
+            statement.executeUpdate(query2);
+
         } catch (final SQLException theEvent) {
             theEvent.printStackTrace();
             System.exit(0);
         }
-        System.out.println("Created inventory table successfully");
+
     }
 
     /**
      * This method inserts data into the tables.
+     * This method may incremet the count of items if they are the same item
+     *
+     * @param theName name of an item to add
+     * @param theType Type of an item to add
+     * 
+     * Source from: https://www.sqlitetutorial.net/sqlite-java/insert/
      */
-    public void insertData() {
-        System.out.println("Attempting to insert two rows into inventory table");
-        String query = "INSERT INTO inventory (NAME, TYPE) VALUES ('Sword', 'Weapon')";
-        try (Connection connection = ds.getConnection();
-             Statement statement = connection.createStatement()) {
-            int rv = statement.executeUpdate(query);
-            System.out.println("executeUpdate() returned " + rv);
+    public void insert(final String theName, final String theType) {
+
+        String query = "INSERT INTO inventory (NAME, TYPE) VALUES (?, ?)";
+
+        try (Connection connection = myDs.getConnection();
+             PreparedStatement prepStatement = connection.prepareStatement(query)) {
+                
+            prepStatement.setString(1, theName);
+            prepStatement.setString(2, theType);
+            prepStatement.executeUpdate();
+
         } catch (final SQLException theEvent) {
             theEvent.printStackTrace();
             System.exit(0);
         }
-        System.out.println("Inserted data into inventory table successfully");
     }
 
     /**
-     * This method selects all data from the tables.
+     * Updates values in the corresponding database.
+     * 
+     * @param theName
+     * @param theType
      */
-    public void selectData() {
-        System.out.println("Selecting all rows from inventory table");
+    public void update(final String theName, final String theType) {
+
+        //TODO: Check if the below query statement is correct
+
+        String query = "UPDATE inventory SET NAME = ? , " +
+                        "VALUE = ?";
+
+        try (Connection connection = myDs.getConnection();
+             PreparedStatement prepStatement = connection.prepareStatement(query)) {
+
+            // sets the corresponding values
+            prepStatement.setString(1, theName);
+            prepStatement.setString(2, theType);
+
+            prepStatement.executeUpdate();
+
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Removes an item from the inventory
+     * @param theItem
+     * 
+     * https://www.sqlitetutorial.net/sqlite-java/delete/
+     */
+    public void remove(final String theItem) {
+
+        String query = "DELETE FROM inventory WHERE NAME = ?";
+
+        try (Connection connection = myDs.getConnection();
+                PreparedStatement prepStatement = connection.prepareStatement(query)) {
+
+                prepStatement.setString(1, theItem);
+
+                prepStatement.executeUpdate();
+                
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Resets all items in the inventory
+     */
+    public void reset() {
+
+    }
+
+    /**
+     * Selects all items stored in the table. I am not sure if data from both table gets selected.
+     * 
+     * Source: https://www.sqlitetutorial.net/sqlite-java/select/
+     */
+    public void selectAll() {
+
+        // Need to check if the below query statement is correct.
         String query = "SELECT * FROM inventory";
-        try (Connection connection = ds.getConnection();
-             Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery(query);
+
+        try (Connection connection = myDs.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(query);) {
+            
             while (rs.next()) {
-                System.out.println("ID: " + rs.getInt("ID"));
-                System.out.println("Name: " + rs.getString("NAME"));
-                System.out.println("Type: " + rs.getString("TYPE"));
+                System.out.println("ID: " + rs.getInt("ID") + "\t" +
+                                    "Name: " + rs.getString("NAME") + "\t" +
+                                    "Type: " + rs.getString("TYPE"));
             }
         } catch (final SQLException theEvent) {
             theEvent.printStackTrace();
             System.exit(0);
         }
-        System.out.println("Selected all data from inventory table successfully");
     }
 
 
