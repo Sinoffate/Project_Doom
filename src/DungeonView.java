be49fb50c292453be4c597e068ca85e3f4ce5be1
sebur <seburoh@outlook.com>
2022-11-19 19:01:15 -0800

    refactor of DuView, entire class no longer in setupComponents

--- src/DungeonView.java
+++ src/DungeonView.java
@@ -1,6 +1,8 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -8,127 +10,122 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serial;
 import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
- * Console based view for Dungeons.
+ * GUI based view for Dungeons.
  * @author james deal
- * @version 0.1
+ * @version 0.2
  */
 public class DungeonView extends JPanel implements PropertyChangeListener {
 
-    /** Character used for top/bottom of room display. */
-    private static final String CEILING_TILE = "-";
-    /** Character used for sides of room display. */
-    private static final String WALL_TILE = "|";
-    /** Character used for player in room display. */
-    private static final char PLAYER_TILE = 'P';
-    /** Character used for empty room in room display. */
-    private static final char EMPTY_TILE = ' ';
+    @Serial
+    private static final long serialVersionUID = 4L;
+
+    /** Image used for top/bottom of room display. */
+    private static ImageIcon ROOM_TILE;
+    /** Image used for player in room display. */
+    private static ImageIcon PLAYER_TILE;
 
     /** Size of dungeon to display. */
     private final int myDungeonSize;
 
-    @Serial
-    private static final long serialVersionUID = 4L;
+    /** GUI Map Panel. */
+    private JPanel myMapPanel;
+    /** GUI GridBag Constraints. */
+    private GridBagConstraints myGBC;
+    /** GUI collection of Map's Room Labels. */
+    private Map<Point, JLabel> myMapLabels;
+    /** GUI Map's Player Label. */
+    private JLabel myPlayerLabel;
 
     /**
      * Default constructor to ready the view.
      * @param theSize dimension of square dungeon.
      * @param thePlayer starting position of the player.
      */
-    public DungeonView(final int theSize, final Point thePlayer) throws IOException {
+    public DungeonView(final int theSize, final Point thePlayer) {
         super();
         myDungeonSize = theSize;
-        //drawMap(thePlayer);
+        loadImages();
         setupComponents();
+        placePlayer(thePlayer);
+        placePlayer(new Point(1,1));
     }
 
-    private void setupComponents() throws IOException {
-        setLayout(new BorderLayout());
-
-        JPanel mapPanel = new JPanel();
-        add(new JLabel("Doomguy stats: Idk prob dead"), BorderLayout.NORTH);
-        add(new JLabel("Bite me"), BorderLayout.SOUTH);
-        add(new JLabel("Menu Options: Die"), BorderLayout.EAST);
-
-
-        mapPanel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        add(mapPanel, BorderLayout.WEST);
-
-        HashMap<Point,JLabel> mapPoints = new HashMap<>();
-
+    /**
+     * Load file images in to use in GUI.
+     */
+    private void loadImages() {
         BufferedImage roomImage = null;
         BufferedImage playerImage = null;
 
         try {
             roomImage = ImageIO.read(new File("fellcleave75.png"));
             playerImage = ImageIO.read(new File("emote75.png"));
-        } catch(IOException ioe) {
+        } catch (final IOException ioe) {
             System.out.println("Unable to fetch image.");
             ioe.printStackTrace();
         }
 
-        ImageIcon roomIcon = new ImageIcon(roomImage);
-        ImageIcon playerIcon = new ImageIcon(playerImage);
-
-        for (int i = 0; i < 5; i++) {
-            JLabel temp;
-            for (int j = 0; j < 5; j++) {
-                temp = new JLabel(roomIcon);
-                mapPoints.put(new Point(i,j),temp);
-                c.gridx = i;
-                c.gridy = j;
-                mapPanel.add(temp,c);
-            }
-        }
-
-        JLabel player = new JLabel(playerIcon);
-        c.gridx = 0;
-        c.gridy = 0;
-        mapPanel.add(player,c);
-        mapPanel.add(mapPoints.get(new Point(0,0)),c);
-
-        mapPanel.setOpaque(true);
-        mapPanel.setBackground(Color.BLUE);
-        this.setOpaque(true);
-        this.setBackground(Color.BLACK);
-
-
+        assert roomImage != null;
+        ROOM_TILE = new ImageIcon(roomImage);
+        assert playerImage != null;
+        PLAYER_TILE = new ImageIcon(playerImage);
     }
 
-    /* Example 3x3 with Player at 0,0
-        -------
-        |P| | |
-        -------
-        | | | |
-        -------
-        | | | |
-        -------
-    */
     /**
-     * Draws map containing player position.
-     * @param thePlayer position of player.
+     * Initial setup of GUI.
      */
-    private void drawMap(final Point thePlayer) {
-        clearScreen();
+    private void setupComponents() {
+        setLayout(new BorderLayout());
 
-        for (int row = 0; row < myDungeonSize; row++) {
-            System.out.println(CEILING_TILE.repeat(myDungeonSize * 2 + 1));
+        //temp so we understand where things go
+        add(new JLabel("Doomguy stats: Idk prob dead"), BorderLayout.NORTH);
+        add(new JLabel("Bite me"), BorderLayout.SOUTH);
+        add(new JLabel("Menu Options: Die"), BorderLayout.EAST);
 
-            for (int col = 0; col < myDungeonSize; col++) {
-                System.out.print(WALL_TILE);
-                if (new Point(row, col).equals(thePlayer)) {
-                    System.out.print(PLAYER_TILE);
-                } else {
-                    System.out.print(EMPTY_TILE);
-                }
+        //setup dungeon map component
+        myMapPanel = new JPanel();  //panel itself
+        myMapPanel.setLayout(new GridBagLayout());  //grid for inside the panel
+        myGBC = new GridBagConstraints();   //ability to use grid effectively
+        add(myMapPanel, BorderLayout.WEST); //put grid in the panel
+        myMapLabels = new HashMap<>();   //ability to reference grid-objects
+
+        //Setup player label.
+        myPlayerLabel = new JLabel(PLAYER_TILE);
+
+        //fill grid with rooms
+        JLabel temp;
+        for (int i = 0; i < myDungeonSize; i++) {
+            for (int j = 0; j < myDungeonSize; j++) {
+                temp = new JLabel(ROOM_TILE);
+                myMapLabels.put(new Point(i, j), temp);
+                myGBC.gridx = i;
+                myGBC.gridy = j;
+                myMapPanel.add(temp, myGBC);
             }
-            System.out.print(WALL_TILE + "\n");
         }
 
-        System.out.println(CEILING_TILE.repeat(myDungeonSize * 2 + 1));
+        myMapPanel.setOpaque(true);
+        myMapPanel.setBackground(Color.DARK_GRAY);
+        this.setOpaque(true);
+        this.setBackground(Color.GRAY);
+    }
+
+    /**
+     * Draw player in designated spot. Also redraws dungeon tile.
+     * @param thePosition position to draw at.
+     */
+    private void placePlayer(final Point thePosition) {
+        myGBC.gridx = thePosition.x;
+        myGBC.gridy = thePosition.y;
+        myMapPanel.add(myPlayerLabel, myGBC);
+        myMapPanel.add(myMapLabels.get(thePosition), myGBC);
     }
 
     /**
@@ -139,35 +136,8 @@ public class DungeonView extends JPanel implements PropertyChangeListener {
     @Override
     public void propertyChange(final PropertyChangeEvent theEvt) {
         if (Dungeon.HERO_POS.equals(theEvt.getPropertyName())) {
-            drawMap((Point) theEvt.getNewValue());
-//            Point newSpot = (Point) theEvt.getNewValue();
-//            c.gridx = newSpot.getX();
-//            c.gridy = newSpot.getY();
-//            mapPanel.add(myPlayer,c);
+            placePlayer((Point) theEvt.getNewValue());
         }
     }
 
-    //utilities
-
-    /**
-     * Clears the terminal by invoking the environment's clear command.
-     * Differs between Windows and Unix
-     * <p>
-     * <a href="https://stackoverflow.com/questions/2979383/java-clear-the-console">Dealing with OS's</a>
-     * <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/System.html#getenv-java.lang.String-">What is Term?</a>
-     */
-    public static void clearScreen() {
-        try { //windows
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        } catch (final Exception e) { //mac and linux
-            try {
-                final String term = System.getenv("TERM");
-                if (term != null && !"dumb".equals(term)) {
-                    new ProcessBuilder("clear").inheritIO().start().waitFor();
-                }
-            } catch (final Exception ignored) {
-                //guess we don't clear the screen today, oh well
-            }
-        }
-    }
 }
