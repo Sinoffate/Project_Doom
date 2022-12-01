5bd8fb3c9914aae901e960fea1277768f968521c
hyunggilwoo <hyunggilswoo@gmail.com>
2022-11-30 20:46:18 -0800

    Added programs to test for database
    removed a duplicate testRoom class.
    Created a TestDatabase class

---
+++ src/Database.java
@@ -0,0 +1,180 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.sqlite.SQLiteDataSource;
+
+/**
+ * This class sets up SQLite database. Database should be accessed by the Inventory and Hero.
+ * Inventory should be accessed by the entire inventory and Hero.
+ * 
+ * @author Jered Wiegel, Hyunggil Woo
+ * @version 1.1
+ */
+public class Database {
+
+    //TODO: a
+    private SQLiteDataSource myDs = null;
+
+    /**
+     * Constructor for Database.
+     * Database will contain non-null references.
+     */
+    public Database() {
+        try {
+            myDs = new SQLiteDataSource();
+            myDs.setUrl("jdbc:sqlite:Project_Doom.db");
+        } catch (final Exception theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        createTables(); 
+    }
+
+    //TODO: Weapons.
+    /**
+     * This method creates the tables in the database.
+     */
+    public void createTables() {
+        
+        //TODO: Does the number of items automatically increment?
+        String query = "CREATE TABLE IF NOT EXISTS inventory ( " +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME TEXT NOT NULL, " +
+                "TYPE TEXT NOT NULL )";
+
+        // query to create a table for a player
+        String query2 = "CREATE TABLE IF NOT EXISTS player ( " +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME TEXT NOT NULL, " +
+                "HEALTH INTEGER NOT NULL, " +
+                "INVENTORY_ID INTEGER NOT NULL, " +
+                "FOREIGN KEY (INVENTORY_ID) REFERENCES inventory(ID) )";
+
+
+        try (Connection connection = myDs.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(query);
+            statement.executeUpdate(query2);
+
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * This method inserts data into the tables.
+     * This method may incremet the count of items if they are the same item
+     *
+     * @param theName name of an item to add
+     * @param theType Type of an item to add
+     * 
+     * Source from: https://www.sqlitetutorial.net/sqlite-java/insert/
+     */
+    public void insert(final String theName, final String theType) {
+
+        String query = "INSERT INTO inventory (NAME, TYPE) VALUES (?, ?)";
+
+        try (Connection connection = myDs.getConnection();
+             PreparedStatement prepStatement = connection.prepareStatement(query)) {
+                
+            prepStatement.setString(1, theName);
+            prepStatement.setString(2, theType);
+            prepStatement.executeUpdate();
+
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Updates values in the corresponding database.
+     * 
+     * @param theName
+     * @param theType
+     */
+    public void update(final String theName, final String theType) {
+
+        //TODO: Check if the below query statement is correct
+
+        String query = "UPDATE inventory SET NAME = ? , " +
+                        "VALUE = ?";
+
+        try (Connection connection = myDs.getConnection();
+             PreparedStatement prepStatement = connection.prepareStatement(query)) {
+
+            // sets the corresponding values
+            prepStatement.setString(1, theName);
+            prepStatement.setString(2, theType);
+
+            prepStatement.executeUpdate();
+
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Removes an item from the inventory
+     * @param theItem
+     * 
+     * https://www.sqlitetutorial.net/sqlite-java/delete/
+     */
+    public void delete(final String theItem) {
+
+        String query = "DELETE FROM inventory WHERE NAME = ?";
+
+        try (Connection connection = myDs.getConnection();
+                PreparedStatement prepStatement = connection.prepareStatement(query)) {
+
+                prepStatement.setString(1, theItem);
+
+                prepStatement.executeUpdate();
+                
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Resets all items in the inventory
+     */
+    public void reset() {
+
+    }
+
+    /**
+     * Selects all items stored in the table. I am not sure if data from both table gets selected.
+     * 
+     * Source: https://www.sqlitetutorial.net/sqlite-java/select/
+     */
+    public void selectAll() {
+
+        // Need to check if the below query statement is correct.
+        String query = "SELECT * FROM inventory";
+
+        try (Connection connection = myDs.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(query);) {
+            
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("ID") + "\t" +
+                                    "Name: " + rs.getString("NAME") + "\t" +
+                                    "Type: " + rs.getString("TYPE"));
+            }
+        } catch (final SQLException theEvent) {
+            theEvent.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+
+}
--- src/testInventory.java
+++ src/testInventory.java
@@ -1,178 +1,37 @@
-////import static org.junit.Assert.assertEquals;
-////
-////import java.util.ArrayList;
-////import java.util.Arrays;
-////import java.util.List;
-////import java.util.Set;
-////import java.util.TreeSet;
-////
-////import org.junit.Before;
-////import org.junit.BeforeClass;
-////import org.junit.Test;
-////
-/////**
-//// *
-//// * Testing if the Inventory would return a correct output.
-//// */
-////
-////public class testInventory {
-////    Set<String> itemList;
-////    Inventory itemInventory;
-////
-////    @Before
-////    public void initalizeDefaultInventory() {
-////        itemList = new TreeSet<>(){"Potion", "Pillar", "BFG"};
-////        itemInventory = new Inventory(itemList);
-////    }
-////
-////
-////    /**
-////     * Checks if the initial inventory contains nothing.
-////     */
-////    @Test
-////    public void testDefaultConstructor() {
-////        initalizeDefaultInventory();
-////        assertEquals("Potion:1, Pillar:1, BFG:1", itemInventory.toString());
-////    }
-////
-////    /**
-////     * Tests if the Inventory is correctly added when it is called.
-////     */
-////    @Test
-////    public void testAdd_OneItem() {
-////        initalizeDefaultInventory();
-////        itemInventory.addItem("Chain_Gun");
-////        assertEquals("Potion, Pillar, BFG, Chain_Gun",itemInventory.toString());
-////    }
-////
-////
-////    @Test
-////    public void testRemove_FirstItem(){
-////        initalizeDefaultInventory();
-////        itemInventory.removeItem("Potion");
-////        assertEquals("Pillar, BFG", itemInventory.toString());
-////    }
-////
-////    /**
-////     * Tests if item is correctly removed from the list.
-////     */
-////    @Test
-////    public void testRemove_LastItem() {
-////        initalizeDefaultInventory();
-////        itemInventory.removeItem("BFG");
-////        assertEquals("Potion, Pillar", itemInventory.toString());
-////    }
-////
-////    /**
-////     * Test if Inventory correctly displays the correct item listed in the inventory.
-////     */
-////    @Test
-////    public void testContains_FirstItem() {
-////        initalizeDefaultInventory();
-////        assertEquals(true, itemInventory.containsItem("Potion"));
-////    }
-////
-////    @Test
-////    public void testContains_MiddleItem() {
-////        initalizeDefaultInventory();
-////        assertEquals(true, itemInventory.containsItem("Pillar"));
-////    }
-////
-////    /**
-////     * Test if Inventory correctly displays the correct item listed in the inventory.
-////     */
-////    @Test
-////    public void testContains_LastItem() {
-////        initalizeDefaultInventory();
-////        assertEquals(true, itemInventory.containsItem("BFG"));
-////    }
-////
-////}
 //import static org.junit.Assert.assertEquals;
 //
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.TreeSet;
 //
 //import org.junit.Before;
-//
+//import org.junit.BeforeClass;
 //import org.junit.Test;
 //
 ///**
 // *
 // * Testing if the Inventory would return a correct output.
-// * Testing to see if inventory can store itesm for the hero but also items for each room and monsters in each room
-// * @author Hyunggil Woo
-// * Version: 1.2
-// * Date: November 16, 2022
-// * @param <E>
 // */
 //
 //public class testInventory {
-//
-//
+//    Set<String> itemList;
 //    Inventory itemInventory;
 //
-//
-//    /** Hard coded items to be used for items */
-//    private final int MAX_ITEM_NUMBER = 5;
-//
-//    private String[] ITEMS = {"Potion", "Pillar", "BFG"};
-//
-//
-//    /**
-//     * Generates a map of the list of items that will be stored in each inventory.
-//     * It will generate a random number number of items in the list
-//     *
-//     * @param E[] theList of objects stored in Database.
-//     *         int theMax maximum number of objects.
-//     * @requires: Item will not contain null;
-//     * @throws: If itemList does not contain null, will not add the thing in the list
-//     * @spec.modifies: Item[]
-//     * @spec.effect: Generates a TreeMap of Item and a corresponding number of it.
-//     * @return count of items generated from the list
-//     */
-//    private Map<String, Integer> generateInventory(final String[] theList, final int theMax) {
-//
-//        // checks if itemList contains Null
-//        for (int i = theList.length - 1; i > 0; i--) {
-//            assert theList[i] == null;
-//        }
-//
-//        Map< String , Integer > inventory = new TreeMap<>();
-//
-//        // choose a random number between 1 - theMax
-//        int numberOfItems = DiceRoll.nextInt(theMax) + 1;
-//        int itemOption = theList.length;
-//
-//        // add items in the list with a random in a map
-//        for (int i = 0; i < numberOfItems; i++) {
-//
-//            // randomly choose an thing from the list
-//            String randomItem = theList[DiceRoll.nextInt(itemOption)];
-//
-//            // add new word in a Item list
-//            if (!inventory.containsKey(randomItem)) {
-//                inventory.put(randomItem, 1);
-//            } else {
-//                // increment count of thing that already exist
-//                int oldValue = inventory.get(randomItem);
-//                inventory.put(randomItem, oldValue + 1);
-//            }
-//        }
-//        return inventory;
+//    @Before
+//    public void initalizeDefaultInventory() {
+//        itemList = new TreeSet<>(){"Potion", "Pillar", "BFG"};
+//        itemInventory = new Inventory(itemList);
 //    }
 //
-//    @Test
-//    public void testGenerateInventory() {
-//                /** Hard coded items to be used for items */
-//        final int MAX_ITEM_NUMBER = 5;
 //
-//        String[] ITEMS = {"Potion", "Pillar", "BFG"};
-//
-//    }
 //    /**
 //     * Checks if the initial inventory contains nothing.
 //     */
 //    @Test
 //    public void testDefaultConstructor() {
+//        initalizeDefaultInventory();
 //        assertEquals("Potion:1, Pillar:1, BFG:1", itemInventory.toString());
 //    }
 //
@@ -181,6 +40,7 @@
 //     */
 //    @Test
 //    public void testAdd_OneItem() {
+//        initalizeDefaultInventory();
 //        itemInventory.addItem("Chain_Gun");
 //        assertEquals("Potion, Pillar, BFG, Chain_Gun",itemInventory.toString());
 //    }
@@ -188,6 +48,7 @@
 //
 //    @Test
 //    public void testRemove_FirstItem(){
+//        initalizeDefaultInventory();
 //        itemInventory.removeItem("Potion");
 //        assertEquals("Pillar, BFG", itemInventory.toString());
 //    }
@@ -197,6 +58,7 @@
 //     */
 //    @Test
 //    public void testRemove_LastItem() {
+//        initalizeDefaultInventory();
 //        itemInventory.removeItem("BFG");
 //        assertEquals("Potion, Pillar", itemInventory.toString());
 //    }
@@ -206,12 +68,14 @@
 //     */
 //    @Test
 //    public void testContains_FirstItem() {
-//        assertEquals(true, itemInventory.contains("Potion"));
+//        initalizeDefaultInventory();
+//        assertEquals(true, itemInventory.containsItem("Potion"));
 //    }
 //
 //    @Test
 //    public void testContains_MiddleItem() {
-//        assertEquals(true, itemInventory.contains("Pillar"));
+//        initalizeDefaultInventory();
+//        assertEquals(true, itemInventory.containsItem("Pillar"));
 //    }
 //
 //    /**
@@ -219,7 +83,143 @@
 //     */
 //    @Test
 //    public void testContains_LastItem() {
-//        assertEquals(true, itemInventory.contains("BFG"));
+//        initalizeDefaultInventory();
+//        assertEquals(true, itemInventory.containsItem("BFG"));
 //    }
 //
 //}
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+/**
+ *
+ * Testing if the Inventory would return a correct output.
+ * Testing to see if inventory can store itesm for the hero but also items for each room and monsters in each room
+ * @author Hyunggil Woo
+ * Version: 1.2
+ * Date: November 16, 2022
+ * @param <E>
+ */
+
+public class testInventory {
+
+
+    Inventory itemInventory; 
+
+
+    /** Hard coded items to be used for items */
+    private final int MAX_ITEM_NUMBER = 5;
+
+    private String[] ITEMS = {"Potion", "Pillar", "BFG"};
+  
+    
+    /**
+     * Generates a map of the list of items that will be stored in each inventory.
+     * It will generate a random number number of items in the list
+     * 
+     * @param E[] theList of objects stored in Database.
+     *         int theMax maximum number of objects. 
+     * @requires: Item will not contain null; 
+     * @throws: If itemList does not contain null, will not add the thing in the list
+     * @spec.modifies: Item[]
+     * @spec.effect: Generates a TreeMap of Item and a corresponding number of it.
+     * @return count of items generated from the list
+     */
+    private Map<String, Integer> generateInventory(final String[] theList, final int theMax) {
+        
+        // checks if itemList contains Null
+        for (int i = theList.length - 1; i > 0; i--) {
+            assert theList[i] == null; 
+        }
+
+        Map< String , Integer > inventory = new TreeMap<>();
+
+        // choose a random number between 1 - theMax
+        int numberOfItems = DiceRoll.nextInt(theMax) + 1;
+        int itemOption = theList.length;
+        
+        // add items in the list with a random in a map
+        for (int i = 0; i < numberOfItems; i++) {
+            
+            // randomly choose an thing from the list
+            String randomItem = theList[DiceRoll.nextInt(itemOption)];
+            
+            // add new word in a Item list
+            if (!inventory.containsKey(randomItem)) {
+                inventory.put(randomItem, 1);
+            } else {
+                // increment count of thing that already exist
+                int oldValue = inventory.get(randomItem);
+                inventory.put(randomItem, oldValue + 1);
+            }
+        }
+        return inventory;
+    }
+
+    @Test
+    public void testGenerateInventory() {
+                /** Hard coded items to be used for items */
+        final int MAX_ITEM_NUMBER = 5;
+
+        String[] ITEMS = {"Potion", "Pillar", "BFG"};
+
+    }
+    /**
+     * Checks if the initial inventory contains nothing.
+     */
+    @Test
+    public void testDefaultConstructor() {
+        assertEquals("Potion:1, Pillar:1, BFG:1", itemInventory.toString());
+    }
+
+    /**
+     * Tests if the Inventory is correctly added when it is called.
+     */
+    @Test
+    public void testAdd_OneItem() {
+        itemInventory.addItem("Chain_Gun");
+        assertEquals("Potion, Pillar, BFG, Chain_Gun",itemInventory.toString());
+    }
+
+
+    @Test
+    public void testRemove_FirstItem(){
+        itemInventory.removeItem("Potion");
+        assertEquals("Pillar, BFG", itemInventory.toString());
+    }
+
+    /**
+     * Tests if item is correctly removed from the list.
+     */
+    @Test
+    public void testRemove_LastItem() {
+        itemInventory.removeItem("BFG");
+        assertEquals("Potion, Pillar", itemInventory.toString());
+    }
+
+    /**
+     * Test if Inventory correctly displays the correct item listed in the inventory.
+     */
+    @Test
+    public void testContains_FirstItem() {
+        assertEquals(true, itemInventory.contains("Potion"));
+    }
+
+    @Test
+    public void testContains_MiddleItem() {
+        assertEquals(true, itemInventory.contains("Pillar"));
+    }
+
+    /**
+     * Test if Inventory correctly displays the correct item listed in the inventory.
+     */
+    @Test
+    public void testContains_LastItem() {
+        assertEquals(true, itemInventory.contains("BFG"));
+    }
+
+}
