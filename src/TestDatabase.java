7980dbfa08f2c43cc98c6f8f1b8e98d83776da9d
hyunggilwoo <hyunggilswoo@gmail.com>
2022-12-03 15:15:02 -0800

    Changed the BeforeEach method of test method

--- src/TestDatabase.java
+++ src/TestDatabase.java
@@ -1,11 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestDatabase {
     
-    Database database;
+    private Database database;
 
     @BeforeEach
+    /**
+     * Initialized a table into creating a table of items
+     */
     void setUp() {
-        
+        database = new Database();
+    }
+
+    @Test
+    void createWeapons() {
+
+    }
+
+    @Test
+    void removingOfMoreThanAvailableItemsError() {
+
+    }
+
+    @Test
+    void updatingOfInventoryWithNegativeAmountError() {
+
+    }
+
+    @Test
+    void updatingOfInventoryWithZeroAmountError() {
+
+    }
+
+    @Test
+    void updatingOfInventoryWithNoChangeErrorError() {
+
+    }
+
+
+    @AfterEach
+    void closeConnections() {
+
     }
 }
