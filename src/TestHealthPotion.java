1880e0585d213abcd535d85350409dc065afecf3
Sinoffate <Sinoffate@hotmail.com>
2022-12-07 17:55:53 -0800

    Added TestHealthPotion

---
+++ src/TestHealthPotion.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHealthPotion {
+
+    Item p;
+
+    @BeforeEach
+    void reset() {
+        p = new HealthPotion();
+    }
+
+
+    @Test
+    void testConGood() {
+        p = new HealthPotion();
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Health Potion", p.getName(), "Bad name");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Health Potion", p.toString(), "Bad toString");
+    }
+
+    @Test
+    void testEquals() {
+        Item i = new HealthPotion();
+        HealthPotion p2 = new HealthPotion();
+        assertEquals(p,i,"Item vs HealthPotion instantiation bad");
+        assertEquals(p,p2,"HealthPotion vs HealthPotion instantiation bad");
+        assertEquals(p,p,"Self test bad");
+
+    }
+}
