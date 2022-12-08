fe3c3ed2e8e308d6dda44f513a5504276ccebbd2
Sinoffate <Sinoffate@hotmail.com>
2022-12-07 17:57:05 -0800

    Added TestVisionPotion

---
+++ src/TestVisionPotion.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestVisionPotion {
+
+    Item p;
+
+    @BeforeEach
+    void reset() {
+        p = new VisionPotion();
+    }
+
+    @Test
+    void testConGood() {
+        p = new VisionPotion();
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Vision Potion", p.getName(), "Bad name");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Vision Potion", p.toString(), "Bad toString");
+    }
+
+    @Test
+    void testEquals() {
+        Item i = new VisionPotion();
+        VisionPotion p2 = new VisionPotion();
+        assertEquals(p,i,"Item vs VisionPotion instantiation bad");
+        assertEquals(p,p2,"VisionPotion vs VisionPotion instantiation bad");
+        assertEquals(p,p,"Self test bad");
+
+    }
+}
